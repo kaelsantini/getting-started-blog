3fa6ae2557174e9b3655bcb4843016e40dc93a78
Kael Santini <kaelsantini@hotmail.com>
2020-02-06 19:52:40 -0300

    Migrating to PG database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_02_04_175654) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_175654) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
@@ -36,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_175654) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "cpf", limit: 11
     t.date "birth_date"
-    t.string "gender"
+    t.string "gender", limit: 1
   end
 
   add_foreign_key "comments", "articles"
